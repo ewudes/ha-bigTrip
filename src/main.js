798c2b67e8e1127d10dfc8ddd535b58c439dcf84
ewudes <ewudes@gmail.com>
2020-11-24 00:11:16 +0400

    added event editing module

--- src/main.js
+++ src/main.js
@@ -5,8 +5,9 @@ import { createTripFilters } from "./view/trip-filters.js";
 import { createTripSort } from "./view/trip-sort.js";
 import { createEventList } from "./view/event-list.js";
 import { createEventItem } from "./view/event-item.js";
+import { editEventItem } from "./view/edit-event-item.js";
 
-const COUNT_EVENT = 9;
+const COUNT_EVENT = 3;
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -28,6 +29,8 @@ render(tripEventsElement, createTripSort(), `beforeend`);
 render(tripEventsElement, createEventList(), `beforeend`);
 
 const eventsList = pageMainElement.querySelector(`.trip-events__list`);
+
+render(eventsList, editEventItem(), `beforeend`);
 
 for (let i = 0; i < COUNT_EVENT; i++) {
   render(eventsList, createEventItem(), `beforeend`);
