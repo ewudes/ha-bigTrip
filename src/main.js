29a221dc71574eb9923c977f679fb3a5ff10c0a4
ewudes <ewudes@gmail.com>
2020-12-25 02:43:24 +0400

    minor fixes

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-import { createTripInfo } from "./view/trip-info.js";
-import { createTripCost } from "./view/trip-cost.js";
-import { createTripTabs } from "./view/trip-tabs.js";
-import { createTripFilters } from "./view/trip-filters.js";
-import { createTripSort } from "./view/trip-sort.js";
-import { createEventList } from "./view/event-list.js";
-import { createEventItem } from "./view/event-item.js";
-import { editEventItem } from "./view/edit-event-item.js";
-import { getEvent } from "./mock/event";
+import TripInfo from "./view/trip-info";
+import TripCost from "./view/trip-cost";
+import TripTabs from "./view/trip-tabs";
+import TripFilter from "./view/trip-filters";
+import TripSort from "./view/trip-sort";
+import EventList from "./view/event-list";
+import EventItem from "./view/event-item";
+import EditEventItem from "./view/edit-event-item";
+import NoItem from "./view/no-event-item";
+import {getEvent} from "./mock/event";
 import {render, positionRender} from "./utils";
+import {COUNT_EVENT} from "./const";
 
-const COUNT_EVENT = 15;
 
 const events = new Array(COUNT_EVENT)
   .fill()
@@ -28,23 +29,60 @@ const routeList = Array.from(route).join(` &mdash; `);
 const tripMainElement = document.querySelector(`.trip-main`);
 const pageMainElement = document.querySelector(`.page-main`);
 const tripControlsElement = tripMainElement.querySelector(`.trip-controls`);
+
+render(tripControlsElement, new TripTabs().getElement(), positionRender.AFTERBEGIN);
+render(tripControlsElement, new TripFilter().getElement(), positionRender.BEFOREEND);
+
 const tripEventsElement = pageMainElement.querySelector(`.trip-events`);
 
-render(tripMainElement, createTripInfo(events, routeList), `afterbegin`);
+const renderTripInfo = (item) => {
+  if (item.length === 0) {
+    return;
+  }
 
-const tripInfoElement = tripMainElement.querySelector(`.trip-info`);
+  render(tripMainElement, new TripInfo(item, routeList).getElement(), positionRender.AFTERBEGIN);
+  const tripInfoElement = tripMainElement.querySelector(`.trip-info`);
+  render(tripInfoElement, new TripCost(item).getElement(), positionRender.BEFOREEND);
+};
 
-render(tripInfoElement, createTripCost(events), `beforeend`);
+const renderItem = (listItem, item) => {
+  const itemComponent = new EventItem(item);
+  const itemEditComponent = new EditEventItem(item);
+  const replaceItemToForm = () => {
+    listItem.replaceChild(itemEditComponent.getElement(), itemComponent.getElement());
+  };
+  const replaceFormToItem = () => {
+    listItem.replaceChild(itemComponent.getElement(), itemEditComponent.getElement());
+  };
+  const onEscKeyDown = (evt) => {
+    if (evt.key === `Escape` || evt.key === `Esc`) {
+      evt.preventDefault();
+      replaceFormToItem();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    }
+  };
+  itemComponent.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, () => {
+    replaceItemToForm();
+    document.addEventListener(`keydown`, onEscKeyDown);
+  });
+  itemEditComponent.getElement().addEventListener(`submit`, (evt) => {
+    evt.preventDefault();
+    replaceFormToItem();
+    document.removeEventListener(`keydown`, onEscKeyDown);
+  });
+  render(listItem, itemComponent.getElement(), positionRender.BEFOREEND);
+};
 
-render(tripControlsElement, createTripTabs(), `beforeend`);
-render(tripControlsElement, createTripFilters(), `beforeend`);
-render(tripEventsElement, createTripSort(), `beforeend`);
-render(tripEventsElement, createEventList(), `beforeend`);
+const renderList = (itemContainer, items) => {
+  const listItem = new EventList();
+  if (items.length === 0) {
+    render(itemContainer, new NoItem().getElement(), positionRender.AFTERBEGIN);
+    return;
+  }
+  renderTripInfo(items);
+  render(itemContainer, new TripSort().getElement(), positionRender.AFTERBEGIN);
+  render(itemContainer, listItem.getElement(), positionRender.BEFOREEND);
+  items.forEach((item) => renderItem(listItem.getElement(), item));
+};
 
-const eventsList = pageMainElement.querySelector(`.trip-events__list`);
-
-render(eventsList, editEventItem(events[0]), `beforeend`);
-
-for (let i = 0; i < COUNT_EVENT; i++) {
-  render(eventsList, createEventItem(events[i]), `beforeend`);
-}
+renderList(tripEventsElement, events);
